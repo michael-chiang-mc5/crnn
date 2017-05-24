751bb55b176a22e9a95db0fe9e58fb8382afab75
Ubuntu <ubuntu@ip-172-31-0-126.us-west-1.compute.internal>
2017-05-24 05:44:51 +0000

    mc-runCRNN should work now (untested on g2)

--- mc/mc-runCRNN.py
+++ mc/mc-runCRNN.py
@@ -1,26 +1,29 @@
 import urllib
 import subprocess
+import requests, json
 
-#interface_url = "http://104.131.145.75/"
-interface_url = "http://127.0.0.1:8000/"
+
+interface_url = "http://104.131.145.75/"
+#interface_url = "http://127.0.0.1:8000/"
 metadata = interface_url + "ImagePicker/listBoundingBoxMetadata/"
 data = urllib.urlopen(metadata)
 
 for line in data: # each line is pk, image_url
-    print line
+    line = line.decode("utf-8").split('\t')
     pk=line[0]
     image_url=line[1]
-    image_save_path = "/Users/mcah5a/Desktop/projects/crnn/mc/temp.jpg"
+    image_save_path = "/home/ubuntu/crnn/mc/temp.jpg"
     urllib.urlretrieve(image_url, image_save_path)
-
-    result = subprocess.check_output(['th', 'mc-demo.lua'])
-
+    
+    print "running on " + image_url
+    
+    result = subprocess.check_output(['th', 'mc-lexiconFree.lua'])
+    #result="text1\traw\n"
     #
     pk = pk # silly
     method = 'crnn-lexiconFree'
     text = result.split('\t')[0]
     raw = result.split('\t')[1] # notes in POST
-
     # post to interface
     payload = {'pk':pk, 'method':method, 'text':text, 'notes':raw }
     post_url = interface_url + "ImagePicker/postOCR/"

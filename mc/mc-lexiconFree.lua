cc8da3a68e2b9ef9f639de45d9ea8fa2700a3bdb
Michael Chiang <michaelchiang@Michaels-iMac.local>
2017-05-30 11:58:28 -0700

    crnn should run faster (untested)

--- mc/mc-lexiconFree.lua
+++ mc/mc-lexiconFree.lua
@@ -31,9 +31,40 @@ local snapshot = torch.load(modelLoadPath)
 loadModelState(model, snapshot)
 model:evaluate()
 
-local imagePath = '/home/ubuntu/crnn/mc/temp.jpg'
-local img = loadAndResizeImage(imagePath)
-local text, raw = recognizeImageLexiconFree(model, img)
+-- get bounding box metadata
+local http = require("socket.http")
+local body, code = http.request("http://104.131.145.75/ImagePicker/list_crnn_metadata/")
+if not body then error(code) end
 
--- store results to temporary text file so mc-runCRNN.py can read --
-print(text .. '\t' .. raw)
+-- split metadata into lines
+lines=(body):split('\n')
+
+-- output file
+local output = io.open("output.txt", "w")
+
+
+-- iterate
+for i=1,#lines,1 do
+   line=lines[i]
+   column = (line):split('\t')
+   pk = column[1]
+   url = column[2]
+
+   -- open image
+   print(url)
+   local body, code = http.request(url)
+   if not body then error(code) end
+
+   -- save to file
+   local f = assert(io.open('temp.jpg', 'wb'))
+   f:write(body)
+   f:close()
+
+   -- TODO: test whether we can do away with reload
+   local img = loadAndResizeImage('temp.jpg')
+   local text, raw = recognizeImageLexiconFree(model, img)
+
+   -- write output to text file
+   output:write(pk .. "\t" .. "crnn-lexiconFree\t" .. text .. "\t" .. raw .. "\n")
+
+end
--- mc/mc-runCRNN.py
+++ mc/mc-runCRNN.py
@@ -1,30 +1,21 @@
 import urllib
 import subprocess
 import requests, json
-
-
+import csv
 interface_url = "http://104.131.145.75/"
-#interface_url = "http://127.0.0.1:8000/"
-metadata = interface_url + "ImagePicker/listBoundingBoxMetadata/"
-data = urllib.urlopen(metadata)
 
-for line in data: # each line is pk, image_url
-    line = line.decode("utf-8").split('\t')
-    pk=line[0]
-    image_url=line[1]
-    image_save_path = "/home/ubuntu/crnn/mc/temp.jpg"
-    urllib.urlretrieve(image_url, image_save_path)
-    
-    print "running on " + image_url
-    
-    result = subprocess.check_output(['th', 'mc-lexiconFree.lua'])
-    #
-    print result
-    pk = pk # silly
-    method = 'crnn-lexiconFree'
-    text = result.split('\t')[0]
-    raw = result.split('\t')[1] # notes in POST
-    # post to interface
-    payload = {'pk':pk, 'method':method, 'text':text, 'notes':raw }
-    post_url = interface_url + "ImagePicker/postOCR/"
-    r = requests.post(post_url, data={'json-str':json.dumps(payload)})
+# run lua script that does crnn on all images
+subprocess.check_output(['th', 'mc-lexiconFree.lua'])
+
+# push results to interface
+with open('output.txt', 'r') as f:
+    reader = csv.reader(f)
+    for row in reader:
+        elements = row[0].split('\t')
+        pk = elements[0]
+        method = elements[1]
+        text = elements[2]
+        notes = elements[3]
+        payload = {'pk':pk, 'method':method, 'text':text, 'notes':raw }
+        post_url = interface_url + "ImagePicker/postOCR/"
+        r = requests.post(post_url, data={'json-str':json.dumps(payload)})
